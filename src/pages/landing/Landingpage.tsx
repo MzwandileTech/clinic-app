12fa710e31a9ccd6341c7dfc0a68cce83552acf6
Cecilia Letsau <cecilialetsau25@gmail.com>
2025-11-10 09:25:39 +0200

    Update Landingpage layout and styles

--- src/pages/landing/Landingpage.tsx
+++ src/pages/landing/Landingpage.tsx
@@ -16,7 +16,7 @@ const Landingpage = () => {
         <Hero />
         <Features />
         <Roles />
-        <Screenshots />
+        
         <Pricing />
         <Testimonials />
         <CTA />
